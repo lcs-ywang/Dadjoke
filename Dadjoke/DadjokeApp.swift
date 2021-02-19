3e4ea94519bfaa07a20fec895162db3163fc18d8
yining wang <ywang@lcs.on.ca>
2021-02-19 11:15:23 -0500

    Initial Commit

---
+++ Dadjoke/DadjokeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DadjokeApp.swift
+//  Dadjoke
+//
+//  Created by Yining Wang on 2021-02-19.
+//
+
+import SwiftUI
+
+@main
+struct DadjokeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
